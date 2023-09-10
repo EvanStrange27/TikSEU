06481bd863c5e9e9ebe620bec22af706dfca7922
杨奇 <StrangeYang@outlook.com>
2023-09-10 19:17:51 +0800

    晚饭前休息push

--- game_core.cpp
+++ game_core.cpp
@@ -1,3 +1,4 @@
+#include "pch.h"
 #include "game_core.h"
 using std::string;
 
@@ -8,4 +9,32 @@ void ELM::SetBid(int bid) {
 void ELM::SetXY(int x,int y) {
 	X = x;
 	Y = y;
+}
+
+void ELM::SetType(int type) {
+	ELMType = type;
+}
+
+void ELM::ShowText() {
+
+}
+
+ELM* GM::CreateELM() {
+	return NULL;
+}
+
+void GM::GMStart() {
+
+}
+
+void GM::Load() {
+	
+}
+
+void GM::Exchange(int x1, int y1, int x2, int y2) {
+
+}
+
+void GM::ClearELM(int x, int y) {
+
 }
--- game_core.h
+++ game_core.h
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "TikSEU.h"
+#include "TikSEUDlg.h"
 #include "afxdialogex.h"
 #include "CGame.h"
 #include "DIALOG1.h"
@@ -13,28 +14,55 @@ private:
 	string Text;
 	int X;
 	int Y;
+	int ELMType;
 public:
 	ELM() {
 		Bid = 0;
 		Text = "0";
 		X = 0;
 		Y = 0;
+		ELMType = 1;
 	}
 	~ELM() {};
 	void SetBid(int bid);
 	void SetXY(int x,int y);
+	void SetType(int type);
+	void ShowText();	//为与之匹配的按钮显示文本
 };
 
 class GM
 {
 private:
 	ELM* elm;
-
+	ELM* Pos[10][10];
+	int Score;
 public:
 	GM() {
 		elm = NULL;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j <= 10; j++) {
+				Pos[i][j] = NULL;
+			}
+		}
+		Score = 0;
 	}
 	~GM() {};
-	ELM* CreateELM();
+	ELM* CreateELM();	//创建新元素
+	void GMStart();		//游戏开始
+	/*	游戏开始
+	* 流程
+	* 随机创建81个元素
+	* 按顺序匹配给按钮
+	* 为按钮装载显示名字
+	*/
+	void Load();		//显示名字
+	void Exchange(int x1,int y1,int x2,int y2);	//成功交换两个元素
+	/*	交换元素
+	* 流程
+	* 判断是否含有特效
+	* 判断是否产生新的特效
+	* 调用特定的消除函数
+	*/
+	void ClearELM(int x, int y);	//消除元素
 
 };
