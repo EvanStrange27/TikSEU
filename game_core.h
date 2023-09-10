7a7e6297a34323c98d49ad194df73926596b66c1
杨奇 <StrangeYang@outlook.com>
2023-09-10 18:41:43 +0800

    写了元素类和可能会用到的函数

--- game_core.h
+++ game_core.h
@@ -3,14 +3,24 @@
 #include "afxdialogex.h"
 #include "CGame.h"
 #include "DIALOG1.h"
-#include <cstring>
+#include "string"
+using std::string;
 
 class ELM
 {
 private:
 	int Bid;
-	string 
+	string text;
+	int x;
+	int y;
 public:
-
-
+	ELM() {
+		Bid = 0;
+		text = "0";
+		x = 0;
+		y = 0;
+	}
+	~ELM() {};
+	void SetBid();
+	void SetXY(int intx,int inty);
 };
